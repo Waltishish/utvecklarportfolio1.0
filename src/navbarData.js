f95dfca33195100049e6a6c56213aa2f3a8464a6
anton.walter@yh.nackademin.se <anton.walter@yh.nackademin.se>
2025-02-03 21:01:08 +0100

    fixed for deploy

--- src/navbarData.js
+++ src/navbarData.js
@@ -1,23 +1,20 @@
 export const navbarData = [
     {
         name: 'Hem',
-        url: 'index.html'
+        url: '/utvecklarportfolio1.0/index.html'
     },
-    
     {
         name: 'Projekt',
-        url: 'projects.html'
+        url: '/utvecklarportfolio1.0/projects.html'
     },
-    
     {
         name: 'GitHub',
         url: 'https://github.com/Waltishish',
         iconClass: 'fab fa-github'
     },
-
     {
         name: 'LinkedIn',
         url: 'https://www.linkedin.com/in/anton-walter-9a7797243/',
         iconClass: 'fab fa-linkedin'
     }
-]
+];
